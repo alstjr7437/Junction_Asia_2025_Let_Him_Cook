ff7b78accbdf64ed23cf25f5cee8e9c7d8694496
giljihun <accc45@outlook.com>
2025-08-23 22:52:57 +0900

    Chore: 와치앱 타겟 추가

---
+++ LetHimCook/LetHimCookWatchApp Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LetHimCookWatchApp Watch App
+//
+//  Created by 길지훈 on 8/23/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
