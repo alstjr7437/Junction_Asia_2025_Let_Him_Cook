921b2a0396e023c7a2124c393e448409926d9a30
김민석|Minseok Kim <alstjr7437@naver.com>
2025-08-24 00:01:29 +0900

    feat: 공사 차량 대분류 선택 부분 구현

--- LetHimCook/LetHimCook/Presentation/SubCarSelect/SubCarSelectView.swift
+++ LetHimCook/LetHimCook/Presentation/SubCarSelect/SubCarSelectView.swift
@@ -8,11 +8,13 @@
 import SwiftUI
 
 struct SubCarSelectView: View {
+    let majorCar: MajorCar
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text(majorCar.displayName)
     }
 }
 
 #Preview {
-    SubCarSelectView()
+    SubCarSelectView(majorCar: .crane)
 }
