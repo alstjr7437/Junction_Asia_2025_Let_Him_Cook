c66ef90712fabeb7c1d9fcb5c6e18fa1c178a611
giljihun <accc45@outlook.com>
2025-08-24 02:02:23 +0900

    Feat: 워치 작업 종료 후, 루트뷰 복귀

--- LetHimCook/LetHimCookWatchApp Watch App/WorkEnding/WorkEndingView.swift
+++ LetHimCook/LetHimCookWatchApp Watch App/WorkEnding/WorkEndingView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct WorkEndingView: View {
+    @Environment(\.dismiss) private var dismiss
+    
     var body: some View {
         waitingConnectionView()
     }
@@ -30,7 +32,7 @@ struct WorkEndingView: View {
                 .foregroundStyle(.secondary)
             
             Button {
-                
+                dismiss()
             } label: {
                 Text("OK")
             }
