8e968d3e02dc8e5673ca79da7c7fc27ab6e1bf57
giljihun <accc45@outlook.com>
2025-08-24 06:15:50 +0900

    Style: 온보딩뷰, 이미지추가, 컬러에셋, 폰트 추 가

--- LetHimCook/LetHimCook/Presentation/Onboarding/OnboardingView.swift
+++ LetHimCook/LetHimCook/Presentation/Onboarding/OnboardingView.swift
@@ -12,34 +12,55 @@ struct OnboardingView: View {
     
     var body: some View {
         VStack {
-            Button {
-                router.push(to: .majorCarSelect)
-            } label: {
-                Text("운전자")
-                    .frame(width: 300, height: 125)
-                    .padding()
-                    .background(Color.gray.opacity(0.3))
-                    .cornerRadius(8)
+            HStack {
+                Text("Signalman")
+                    .font(.b4)
+                Spacer()
+                
+                Button{
+                    router.push(to: .record)
+                } label:{
+                    Text("Log")
+                        .foregroundStyle(Color(#colorLiteral(red: 0.5465203524, green: 0.5815119147, blue: 0.5980941057, alpha: 1)))
+                        .font(.sb2)
+                }
             }
             
-            Button {
-                router.push(to: .searchDriver)
-            } label: {
-                Text("신호수")
-                    .frame(width: 300, height: 125)
-                    .padding()
-                    .background(Color.gray.opacity(0.3))
-                    .cornerRadius(8)
+            Spacer()
+            
+            VStack(spacing: 17) {
+                Button {
+                    router.push(to: .majorCarSelect)
+                } label: {
+                    VStack {
+                        Image("operator")
+                        Text("Operator")
+                            .font(.b4)
+                    }
+                    .frame(width: 361, height: 245)
+                    .foregroundStyle(.black)
+                    .background(Color(#colorLiteral(red: 0.9025448561, green: 0.9424672723, blue: 0.9632663131, alpha: 1)))
+                    .cornerRadius(20)
+                }
+                
+                Button {
+                    router.push(to: .searchDriver)
+                } label: {
+                    VStack {
+                        Image("signalman")
+                        Text("Signalman")
+                            .font(.b4)
+                    }
+                    .frame(width: 361, height: 245)
+                    .foregroundStyle(.black)
+                    .background(Color(#colorLiteral(red: 0.9025448561, green: 0.9424672723, blue: 0.9632663131, alpha: 1)))
+                    .cornerRadius(20)
+                }
             }
+            
+            Spacer()
         }
         .padding()
-        .navigationBarItems(trailing:
-            Button{
-                router.push(to: .record)
-            } label:{
-                Text("log")
-            }
-        )
     }
 }
 
--- LetHimCook/LetHimCook/Util/Enum/Font+Extension.swift
+++ LetHimCook/LetHimCook/Util/Enum/Font+Extension.swift
@@ -5,3 +5,57 @@
 //  Created by 김민석 on 8/23/25.
 //
 
+import SwiftUI
+
+extension Font {
+    // Regular
+    /// Regular 14/150
+    static let r1: Font = .custom("Pretendard-Regular", size: 14)      // 150%
+    
+    /// Regular 18/150
+    static let r2: Font = .custom("Pretendard-Regular", size: 18)      // 150%
+    
+    /// Regular 12/150
+    static let r3: Font = .custom("Pretendard-Regular", size: 20)      // 150%
+    
+    /// Regular 12/150
+    static let r4: Font = .custom("Pretendard-Regular", size: 12)      // 150%
+    
+    
+    // Medium
+    /// Medium 16/150
+    static let m1: Font = .custom("Pretendard-Medium", size: 16)    // 150%
+    
+    /// Medium 18/150
+    static let m2: Font = .custom("Pretendard-Medium", size: 18)    // 150%
+    
+    // Semibold
+    /// SemiBold 16/150
+    static let sb1: Font = .custom("Pretendard-Semibold", size: 16) // 150%
+    
+    /// SemiBold 18/150
+    static let sb2: Font = .custom("Pretendard-Semibold", size: 18) // 150%
+    
+    /// SemiBold 20/150
+    static let sb3: Font = .custom("Pretendard-Semibold", size: 20) // 150%
+    
+    /// SemiBold 22/150
+    static let sb4: Font = .custom("Pretendard-Semibold", size: 22) // 150%
+    
+    /// SemiBold 24/150
+    static let sb5: Font = .custom("Pretendard-Semibold", size: 24) // 130%
+    
+    
+    // Bold
+    /// Bold 14/150
+    static let b1: Font = .custom("Pretendard-Bold", size: 14)      // 150%
+    
+    /// Bold 18/150
+    static let b2: Font = .custom("Pretendard-Bold", size: 18)      // 150%
+    
+    /// Bold 24/130
+    static let b3: Font = .custom("Pretendard-Bold", size: 24)      // 130%
+    
+    /// Bold 36/130
+    static let b4: Font = .custom("Pretendard-Bold", size: 36)      // 140%
+}
